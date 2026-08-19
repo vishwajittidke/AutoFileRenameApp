f715e773600a891d3c8b4f6b2977944c9e6ce981
User <user@example.com>
2026-08-19 22:51:13 +0530

    Remove test_exts folder and src debug scripts

--- src/debug_extractor.cpp
+++
@@ -1,9 +0,0 @@
-#include "ContextExtractor.h"
-#include <iostream>
-
-int main() {
-    ContextExtractor extractor;
-    std::wstring name = extractor.ExtractTitleFromPropertySystem(L"C:\\Users\\Vishwajit\\Downloads\\Flight Logo.png");
-    std::wcout << L"EXTRACTED: " << name << std::endl;
-    return 0;
-}
